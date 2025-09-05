edc2a5b2a8501b33af5657c69491c3eb32bacacb
gisella.bronzetti <bronzett@gmail.com>
2025-09-06 01:11:47 +0200

    fix: var env name

--- src/NestedSet/Infrastructure/config/config.php
+++ src/NestedSet/Infrastructure/config/config.php
@@ -1,16 +1,16 @@
 <?php
-$dbName = getenv('DB_NAME') ?: 'test';
+$dbName = getenv('MYSQL_DATABASE') ?: 'test';
 $dbHost = getenv('DB_HOST') ?: 'mysql';
 $dbPort = getenv('DB_PORT') ?: '3306';
-$dbUsername = getenv('DB_USERNAME') ?: 'user';
-$dbPassword = getenv('DB_PASSWORD') ?: 'password';
+$dbUsername = getenv('MYSQL_USER') ?: 'user';
+$dbPassword = getenv('MYSQL_PASSWORD') ?: 'password';
 
 return [
     //"mysql" => [
-        "dbUrl" => "mysql:host={$dbHost};dbname={$dbName};charset=utf8mb4;port={$dbPort}",
-        "dbUsername" => $dbUsername,
-        "dbPassword" => $dbPassword,
-        "dbName" => $dbName,
+    "dbUrl" => "mysql:host={$dbHost};dbname={$dbName};charset=utf8mb4;port={$dbPort}",
+    "dbUsername" => $dbUsername,
+    "dbPassword" => $dbPassword,
+    "dbName" => $dbName,
     //],
 
     "supportedLanguages" => ['english', 'italian'],
